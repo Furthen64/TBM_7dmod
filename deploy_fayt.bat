422d3f9f2233456a9f63c0c259df2969c1ca8c1b
Fayt64 <35999444+Furthen64@users.noreply.github.com>
2024-03-24 13:01:28 +0100

    deploy_fayt.bat deploy script

--- deploy_fayt.bat
+++ deploy_fayt.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 setlocal
-set folderpath="C:\Users\fayt64\AppData\Roaming\7DaysToDie\Mods\"
-set folderdel="C:\Users\fayt64\AppData\Roaming\7DaysToDie\Mods\tbm-mod-a21"
-set foldergithubrepo="C:\github\TBM_7dmod"
+set folderpath="C:\Users\fayt64\AppData\Roaming\7DaysToDie\Mods"
+set foldermod="C:\Users\fayt64\AppData\Roaming\7DaysToDie\Mods\tbm-mod-a21"
+set foldergithubrepo="C:\github\TBM_7dmod\tbm-mod-a21"
 
 REM --- Check if this folder exist
 if exist %folderpath%\ (  
@@ -18,7 +18,7 @@ if exist %foldergithubrepo%\ (
 )
 
 REM -- Should we delete an old mod install?
-if exist %folderdel%\ (
+if exist %foldermod%\ (
   GOTO DELETING_FOLDER
 ) else (
   GOTO COPY_MODFOLDER
@@ -26,14 +26,15 @@ if exist %folderdel%\ (
 
 
 :DELETING_FOLDER
-echo Deleting folder %folderdel% ?
-@RD /s %folderdel% 
+echo Deleting folder %foldermod% ?
+@RD /s %foldermod% 
+mkdir %foldermod%
 
 
 
 :COPY_MODFOLDER
 echo Copying from github repo to to modfolder
-xcopy %foldergithubrepo% %folderpath%
+robocopy /e %foldergithubrepo% %foldermod%
  
 
 :END
